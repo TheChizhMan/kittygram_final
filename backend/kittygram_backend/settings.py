a0b895b00593febdc511a1cf77c7102776977ec3
Igor Chizh <igorchizh@MacBook-Pro.local>
2023-11-05 19:55:13 +0300

    ашчштп

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,9 @@
-# flake8: noqa
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
